8d4637b48978bfc456492975f6ae3868aba59d6d
Payal Nigade <payal@payals-macbook-pro.local>
2018-02-25 22:00:36 +0530

    spotify first commit

---
+++ SpotifyDay6/CustomCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCollectionViewCell.swift
+//  SpotifyDay6
+//
+//  Created by payal on 24/02/18.
+//  Copyright © 2018 Payal Nigade. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var subLabel: UILabel!
+    
+}
---
+++ SpotifyDay6/cutom2CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  cutom2CollectionViewCell.swift
+//  SpotifyDay6
+//
+//  Created by payal on 24/02/18.
+//  Copyright © 2018 Payal Nigade. All rights reserved.
+//
+
+import UIKit
+
+class cutom2CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var selectedButton: UIButton!
+}
